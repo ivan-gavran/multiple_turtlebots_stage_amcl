e0d14daed2139d4c971c54031acab84d75bb7859
gavran <gavran@mpi-sws.org>
2016-08-29 11:29:15 +0200

    sending robot_0 to impossible (taken) location

--- scripts/patrol_organizer.py
+++ scripts/patrol_organizer.py
@@ -13,7 +13,7 @@ class PatrolCoordinator(object):
 #     def __init__(self, availableRobots=['robot_0', 'robot_1'], listOfPositions={'robot_0':[(0, 14),(25,15), (24,14), (23, 15), (26, 13.75)],\
 #                                                                       'robot_1':[(24.5,13.25), (25, 13.25)]}):
 #     
-    def __init__(self, availableRobots=['robot_0', 'robot_1'], listOfPositions={'robot_0':[(1,0), (0,0)], 'robot_1':[ (1,2), (1,1)]}):
+    def __init__(self, availableRobots=['robot_0', 'robot_1'], listOfPositions={'robot_0':[(1,1), (0,0)], 'robot_1':[ (1,2), (1,1)]}):
         rospy.loginfo('coordinator initialization')
         self.nextPositionIndex = {'robot_0':0, 'robot_1':0}
         self.availableRobots = availableRobots
@@ -51,7 +51,7 @@ def main():
     coordinator = PatrolCoordinator(availableRobots=['robot_0', 'robot_1'])
     rospy.init_node('coordinator')
     coordinator.sendDirection('robot_0')
-    coordinator.sendDirection('robot_1')
+    #coordinator.sendDirection('robot_1')
     rospy.spin()
 
 if __name__ == '__main__':
